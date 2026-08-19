ddf4de40ff992bfca70ced2cb321ef4081fa9fc9
Brzezina <artur.brzezina@capgemini.com>
2026-08-19 16:06:57 +0200

    feat: add jwt verification for websocket connection

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -57,4 +57,8 @@ export class AuthService {
       throw new UnauthorizedException('Credentials are not valid.');
     }
   }
+
+  verifyToken(jwt: string) {
+    this.jwtService.verify(jwt);
+  }
 }
