0a5648c82d68a10207ad68af32c06dec34a2f164
Brzezina <artur.brzezina@capgemini.com>
2026-08-19 15:21:32 +0200

    add search params for available products

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -8,6 +8,7 @@ import {
   Param,
   ParseFilePipe,
   Post,
+  Query,
   UploadedFile,
   UseGuards,
   UseInterceptors,
@@ -65,8 +66,8 @@ export class ProductsController {
 
   @Get()
   @UseGuards(JwtAuthGuard)
-  async getProducts() {
-    return this.productsService.getProducts();
+  async getProducts(@Query('status') status?: string) {
+    return this.productsService.getProducts(status);
   }
 
   @Get(':productId')
